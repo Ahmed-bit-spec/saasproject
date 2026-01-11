1944d78e05af0ea22c78dae7fb5357216e3d36df
Ahmed U40 <ahmeduuu500@gmail.com>
2026-01-11 09:17:51 +0300

    fix prisma generate for Vercel

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,23 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-
-    var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
 let prisma: PrismaClient;
 
-if(process.env.NODE_ENV === "production"){
-
-    prisma = new PrismaClient();
-}else{
-
-    if(!globalThis.prisma){
-
-        globalThis.prisma = new PrismaClient();
-    }
-
-    prisma = globalThis.prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient();
+  }
+  prisma = globalThis.prisma;
 }
 
 export default prisma;
