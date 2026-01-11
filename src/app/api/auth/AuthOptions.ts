3a7669df54ee044d45670f59c74552904853006f
Ahmed U40 <ahmeduuu500@gmail.com>
2026-01-11 09:07:42 +0300

    first commit

---
+++ src/app/api/auth/AuthOptions.ts
@@ -0,0 +1,25 @@
+
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../../prisma/client"
+
+export const AuthOptions: NextAuthOptions = {
+
+    secret:process.env.NEXTAUTH_SECRET,
+    providers: [
+
+        GoogleProvider({
+
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+
+
+        })
+    ],
+
+    adapter: PrismaAdapter(prisma)
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+import React from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import { FcGoogle } from 'react-icons/fc'
+import { signIn } from 'next-auth/react'
+
+
+const LoginPage = () => {
+  return (
+    <div className='flex h-screen justify-center items-center'>
+    <Card className='w-87.5 border-0'>
+    <CardHeader>
+        <CardTitle>Login User</CardTitle>
+        <CardDescription>Login with your google account</CardDescription>
+    </CardHeader>
+    <CardContent>
+        <div className='grid w-full items-center gap-4 '>
+            <div className='flex flex-col space-y-1.5'>
+            <Button variant={'outline'} className='border-0'
+            onClick={() => signIn('google',{
+
+              callbackUrl: '/'
+            })}>
+                <FcGoogle className='w-6 h-6'/>
+
+                <span>Continue with google</span>
+                
+            </Button>
+            </div>
+
+        </div>
+    </CardContent>
+  
+    </Card>
+    </div>
+  )
+}
+
+export default LoginPage
